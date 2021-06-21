9fcbef6a1370c4fb34bda6c5e96f7cb426cfec75
suubh <subhdec99@gmail.com>
2021-06-21 14:04:25 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/contentmodest-goodall-5bb984/posts`,
+        path: `${__dirname}/content/posts`,
       },
     },
     {
